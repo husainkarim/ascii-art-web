67487f08a8408ef6d556f3c8d6d7baad2c95bf89
halabboo <husain.akarim@gmail.com>
2024-01-14 20:09:41 +0300

    edit port

--- main.go
+++ main.go
@@ -9,11 +9,11 @@ import (
 func main() {
 	http.HandleFunc("/", server.PathHandler)
 	//Below is address for local host server
-	link := "http://localhost:8000/"
+	link := "http://localhost:8080/"
 	fmt.Println("\033[36mServer Connected...\033[0m")
 	fmt.Printf("\033[36mlink on: %s\033[0m\n", link)
 	http.Handle("/css/", http.FileServer(http.Dir("templates")))
 	http.Handle("/img/", http.FileServer(http.Dir("templates")))
 	http.Handle("/js/", http.FileServer(http.Dir("templates")))
-	http.ListenAndServe(":8000", nil)
+	http.ListenAndServe(":8080", nil)
 }
